8f92e9c0e21a2799a843ed9d31c00cdf56093c96
ashleyyong10 <115581319+ashleyyong10@users.noreply.github.com>
2023-06-01 23:48:16 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(
   cors({
-    origin: ["https://orbital-testwithaaronfrontend.vercel.app", "http://localhost:3000"]
+    origin: "https://orbital-frontend-alpha.vercel.app/login",
   })
 );
 
