600f36b0f25b5da22728d9b75603f1208d736db7
Xanelle <xanelletanls@gmail.com>
2023-06-13 02:12:14 +0800

    minor change

--- frontend/src/features/modules/moduleService.js
+++ frontend/src/features/modules/moduleService.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const DEV_API = "http://localhost:8000";
 const PROD_API = "https://orbital-backend-axsauce.vercel.app";
 
-const API_URL = DEV_API + "/api/modules/";
+const API_URL = PROD_API + "/api/modules/";
 
 // Create new module
 const createModule = async (moduleData, token) => {
